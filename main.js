bd7c7e71af0ab834076017169b285ff2e0717c72
IslamKumbayev <islam-0131@mail.ru>
2022-10-21 18:00:01 +0600

    lab9

---
+++ main.js
@@ -0,0 +1,37 @@
+function perimeter(b, c) { // beginnig of task number 2
+    d = 2 * (b + c)
+    return d
+}
+let b=4
+let c=5
+res = perimeter(b,c)
+console.log("If sides are", b, "and", c, "then perimeter is equal to",res) //task number 2
+
+
+
+let x = -4 //beginnig of task number 9
+if (x>0) {
+    console.log (x,"is positive")
+}
+else if (x==0) {
+    console.log (x,"is zero")
+}
+else {
+    console.log (x ,"is negative")
+} //end of task number 9
+
+
+
+let e = 15
+let f = 10
+if (e>f) {
+    console.log (e, "is bigger than", f)
+}
+else if (e==f) {
+    console.log (e, "is equal to", f)    
+}
+else {
+    console.log (e, "is less than", f)
+}
+
+
